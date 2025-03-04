98484c4a086748f198eae13ccd76b10b200111bb
Dylan Grootenboer <dylancgrooten@gmail.com>
2025-03-04 09:59:24 -0500

    Kyle and Dylan DOG

---
+++ src/Dylan_Kyle_Dog/Animal.py
@@ -0,0 +1,19 @@
+from __future__ import annotations
+
+class Animal:
+    age: int        # This is in seconds
+    name: str
+    gender: str
+    n_legs: int
+
+    def __init__(self: Animal, name: str, gender: str) -> None:
+        # First we create the attributes that came from arguments
+        self.name = name
+        self.gender = gender
+
+        # Then we set the attributes that have default values
+        self.age = 0
+
+        # For n_legs, there may be no default value that makes sense
+        # It is OK for this to have no value; it will cause an error
+        # if someone tries to access it, which is OK.
