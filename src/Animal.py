76dba20a13afc6e7ceaea6c9a3201debd1a7f4e0
Julia <julia_chung@tdchristian.ca>
2025-03-04 10:00:33 -0500

    spelling fixed

--- src/Animal.py
+++ src/Animal.py
@@ -9,7 +9,9 @@ class Animal:
     IQ: int         
     friendliness: int
     speed: int
-    sstubborness: int 
+    stubborness: int 
+    domesticablity_pct: int
+
 
 
     def __init__(self: Animal, name: str, gender: str) -> None:
