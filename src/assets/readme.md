5a18017f63cea9611d6d4d5e104902793ab46f4d
Daniel <daniel.kuzhel@gmail.com>
2025-03-20 09:27:44 -0400

    lol

--- src/assets/readme.md
+++ src/assets/readme.md
@@ -4,4 +4,8 @@
 
 ![alt text](image-2.png)
 
+![alt text](image.png)
 
+![alt text](image-1.png)
+
+![alt text](fish-2.png)
--- src/game/assets.py
+++ src/game/assets.py
@@ -3,7 +3,7 @@ import pygame.image
 # TODO automate animal naming
 
 keys = [
-    'capybara', 'horse', 'elephant', 'cat', 'shark', 'mutt', 'husky', 'chihuahua', 'wolf',
+    'capybara', 'horse', 'elephant', 'cat', 'shark', 'mutt', 'husky', 'chihuahua', 'wolf', 'barracuda'
     # fish
 ]
 
