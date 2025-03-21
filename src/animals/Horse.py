537fb627b7164f2d5ec715014f3bd5fb932208d5
Julia <julia_chung@tdchristian.ca>
2025-03-21 12:25:54 -0400

    horse adding trample

--- src/animals/Horse.py
+++ src/animals/Horse.py
@@ -21,9 +21,35 @@ class Horse(Equine):
         self.size = 110
         self.diet = [Apple]
 
+    def trample(self: Horse, victim: Animal):
+        # Thinking about changing this to be impacted by energy. 
+        # The amount of energy the horse has the more is it able to attack. 
+        # For example, energy is gained through sleep and eating, 
+        # if their energy is above x amount, then they can trample 
+        # but, if not, they have to run away or get hurt or whatever. 
+        # As long as their energy is above x, they can keep trampling 
+        force = random.randint(1, 50)
+        if force < 5: 
+            squash = 'poor'
+            status = 'no injuries'
+        elif force < 15: 
+            squash = 'slightly succesfull'
+            status= 'a little boo boo'
+        elif force < 30: 
+            squash = 'succesfull'
+            status = 'pain'
+        elif force < 45: 
+            squash = 'quite succesfull'
+            status = 'death'
+        else: 
+            squash = 'SUCCESS'
+            status = 'there was an animal there?'
+        print(f'Attack on {victim} was {squash}, they have {status}')
+        
+
     def move(self: Horse, duration: int) -> None:
-       self.walk(duration) 
+      self.walk(duration) 
     def walk(self: Horse, duration: int) -> None:
-    
-    def energy(self: Horse) -> None: 
-        pass 
+       
+
+
