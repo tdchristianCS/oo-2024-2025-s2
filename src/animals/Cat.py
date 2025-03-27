ed90dff96bb954133152d5aa0e9a53eacfd06acf
your <sawczak@tdchristian.ca>
2025-03-27 09:29:16 -0400

    type annotation fix

--- src/animals/Cat.py
+++ src/animals/Cat.py
@@ -51,7 +51,7 @@ class Cat(Mammal):
 
             print(f'{self.name} tripped on {pronoun}')
     
-    def format_info_lines(self: Cat) -> str:
+    def format_info_lines(self: Cat) -> list[str]:
         return [
             f'{self.name} ({self.age} {self.gender})',
             f'Fur: {self.fur_colour}',
