f8749423e238b931e9bcc1a72fb26a316f43cd13
Edwin Jiang <edwinjiang222@gmail.com>
2025-03-28 10:36:54 -0400

    Rodent docstrings

--- src/animals/Rodent.py
+++ src/animals/Rodent.py
@@ -6,10 +6,18 @@ from foods.Food import Meat, Apple
 from game.constants import TERRAIN_LAND
 
 class Rodent(Mammal):
+    """
+    Defines Rodent. 
+    Rodents are filthy, so this gives them a diseases list of strings.
+    Some have tails, so we have a bool for has_tail.
+    """
     diseases: list[str]
     has_tail: bool
     
     def __init__(self: Rodent, args: dict[str, object]) -> None:
+        """
+        Acts as a parent class for all rodents.
+        """
         args['terrains'] = [TERRAIN_LAND]
         
         super().__init__(args)
