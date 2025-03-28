f490ed03f96b64ab4bba9e083326013677044739
Dylan Grootenboer <dylancgrooten@gmail.com>
2025-03-28 10:39:50 -0400

    dog dict str

--- src/animals/Dog.py
+++ src/animals/Dog.py
@@ -7,11 +7,24 @@ from foods.Food import Meat
 from game.constants import TERRAIN_LAND
 
 class Dog(Mammal):
+    """
+    An animal is a type of entity that can move, 
+    eat, interact, die, etc.
+
+    """
+    
+    
     loudness_bark: int
     lung_capacity: int
     diet = [Meat]
 
     def __init__(self: Dog, args: dict[str, object]) -> None:
+        """
+        expected args: 
+            name: str
+            gender: str
+        """
+        
         args['terrains'] = [TERRAIN_LAND]
         super().__init__(args)
         
@@ -23,10 +36,18 @@ class Dog(Mammal):
         self.info['fur_colour'] = self.fur_colour
 
     def move(self: Dog, duration: int) -> None:
+        """
+        Move the animal for a certain duration.
+        This method used the game loop to update the animal's position, 
+        done in seconds
+        """
+    
         self.walk(duration)
 
     def walk(self: Dog, duration: int) -> None:
-
+        """
+        Make the animal walk for a certain duration. 
+        """
         if self.lung_capacity < 0:
             self.lung_capacity = 0
         elif self.lung_capacity > 100:
@@ -40,6 +61,10 @@ class Dog(Mammal):
             print(f'{self.name} is too tired to walk')
 
     def format_info_lines(self: Dog) -> list[str]:
+        """
+        Return a list of info lines about this Dog
+        """
+        
         lines = super().format_info_lines()
         lines.extend([
             f'Fur: {self.fur_colour}',
@@ -52,8 +77,19 @@ class Husky(Dog):
     friendliness = 70
     speed = 70
     size = 35
+    """
+    From data: name, gender
+    Supplied: fur_colour
+    """
 
     def __init__(self: Husky, args: dict[str, object]) -> None:
+        """
+        Create a Husky dog. Sets fur color.
+        Expected args:
+            - name
+            - gender
+        """
+        
         args['fur_colour'] = 'grey'
         super().__init__(args)
 
@@ -68,6 +104,13 @@ class Chihuahua(Dog):
     Supplied: fur_colour
     """
     def __init__(self: Chihuahua, args: dict[str, object]) -> None:
+        """
+        Create a Chihuahua dog. Sets fur color.
+        Expected args:
+            - name
+            - gender
+        """
+        
         args['fur_colour'] = 'beige'
         super().__init__(args)
 
@@ -81,4 +124,12 @@ class Mutt(Dog):
     From data: name, gender, fur_colour
     """
     def __init__(self: Mutt, args: dict[str, object]) -> None:
+        """
+        Create a Mutt dog.
+        Expected args:
+            - name
+            - gender
+            - fur_colour
+        """
+        
         super().__init__(args)
